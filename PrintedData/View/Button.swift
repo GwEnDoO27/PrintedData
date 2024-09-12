cb4f105431c3985ab2696c8fb4bc7818961a0c9a
gbenard <gwendal.benard@gmail.com>
2024-09-12 22:41:45 +0200

    App works // Adding and view last datas

---
+++ PrintedData/View/Button.swift
@@ -0,0 +1,18 @@
+//
+//  Button.swift
+//  PrintedData
+//
+//  Created by Gwendal Benard on 12/09/2024.
+//
+
+import SwiftUI
+
+struct Button: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Button()
+}
---
+++ PrintedData/View/OldsData.swift
@@ -0,0 +1,18 @@
+//
+//  OldsData.swift
+//  PrintedData
+//
+//  Created by Gwendal Benard on 12/09/2024.
+//
+
+import SwiftUI
+
+struct OldsData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OldsData()
+}
