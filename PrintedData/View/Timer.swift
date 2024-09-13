59a526b937abb50f10cf6d601905a7b1a19091f1
gbenard <gwendal.benard@gmail.com>
2024-09-13 21:35:44 +0200

    Textfield timer

---
+++ PrintedData/View/Timer.swift
@@ -0,0 +1,18 @@
+//
+//  Timer.swift
+//  PrintedData
+//
+//  Created by Gwendal Benard on 13/09/2024.
+//
+
+import SwiftUI
+
+struct Timer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Timer()
+}
