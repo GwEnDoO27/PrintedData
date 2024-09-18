102b75afb400b75651ec64663b2109f54c3e2846
gbenard <gwendal.benard@gmail.com>
2024-09-18 13:18:48 +0200

    Stylizing data page

---
+++ PrintedData/View/Notification.swift
@@ -0,0 +1,18 @@
+//
+//  Notification.swift
+//  PrintedData
+//
+//  Created by Gwendal Benard on 17/09/2024.
+//
+
+import SwiftUI
+
+struct Notification: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Notification()
+}
